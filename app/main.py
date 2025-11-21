17b37bc478739ce068f69b1bbaabb5de7a945c03
Sergio <sguillem04@gmail.com>
2025-11-21 23:19:49 +0100

    feat: configura conexión a base de datos SQLite con SQLModel

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 
+from app.db import create_db_and_tables
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup() -> None:
+    """
+    Código que se ejecuta cuando arranca la aplicación.
+    Aquí creamos las tablas en la base de datos.
+    """
+    create_db_and_tables()
 
 
 @app.get("/health")
